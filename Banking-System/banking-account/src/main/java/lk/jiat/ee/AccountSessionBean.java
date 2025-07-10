04d8e8a9d47f4e5b06d5747a4c327eef34212a82
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-11 02:12:11 +0530

    Update AccountSessionBean.java

--- Banking-System/banking-account/src/main/java/lk/jiat/ee/AccountSessionBean.java
+++ Banking-System/banking-account/src/main/java/lk/jiat/ee/AccountSessionBean.java
@@ -1,7 +1,18 @@
 package lk.jiat.ee;
 
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lk.jiat.ee.entity.Account;
+import lk.jiat.ee.service.AccountService;
 
 @Stateless
-public class AccountSessionBean {
+public class AccountSessionBean implements AccountService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void saveAccount(Account account) {
+        em.persist(account);
+    }
 }
