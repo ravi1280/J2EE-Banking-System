17ded2d18c02abd55651e5b8594fdc1fe7eab66f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-11 02:12:47 +0530

    Create addCustomer.java

---
+++ Banking-System/banking-web/src/main/java/lk/jiat/ee/servlet/addCustomer.java
@@ -0,0 +1,50 @@
+package lk.jiat.ee.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.entity.Customer;
+import lk.jiat.ee.service.CustomerService;
+import lk.jiat.ee.utils.Encyptions;
+
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/addCustomer")
+public class addCustomer extends HttpServlet {
+
+    @EJB
+    private CustomerService customerService;
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String fullName = request.getParameter("fullName");
+        String address = request.getParameter("address");
+        String phone = request.getParameter("phone");
+
+        String encryptPassword = Encyptions.encrypt(password);
+        String verificationCode = UUID.randomUUID().toString();
+        Customer customer = new Customer(fullName, email, encryptPassword, phone, address);
+        customer.setVerificationCode(verificationCode);
+
+        customerService.createCustomer(customer);
+        //roll back krnn customer add une nethnm
+
+        response.sendRedirect(request.getContextPath()+"/admin/addCustomers.jsp");
+
+
+//        VerificationMail verificationMail = new VerificationMail(email, verificationCode);
+//        MailServiceProvider.getInstance().sendMail(verificationMail);
+//
+//        response.sendRedirect(request.getContextPath()+"/login.jsp");
+
+
+
+    }
+}
