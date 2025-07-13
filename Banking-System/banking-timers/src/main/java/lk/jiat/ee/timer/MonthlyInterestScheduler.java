9996aedac3214320accfdaaff85fc582038b6e24
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-13 15:11:49 +0530

    Create MonthlyInterestScheduler.java

---
+++ Banking-System/banking-timers/src/main/java/lk/jiat/ee/timer/MonthlyInterestScheduler.java
@@ -0,0 +1,31 @@
+package lk.jiat.ee.timer;
+
+
+import jakarta.ejb.*;
+import lk.jiat.ee.entity.Account;
+import lk.jiat.ee.service.AccountService;
+
+import java.util.List;
+
+@Singleton
+public class MonthlyInterestScheduler {
+
+    @EJB
+    private AccountService accountService;
+
+//    @Schedule(dayOfMonth = "Last", hour = "23", minute = "59", persistent = false)
+//    @Schedule(minute = "*/2", hour = "*", persistent = false)
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void updateMonthlyInterest() {
+        List<Account> accounts  = accountService.getAllAccounts();
+
+        for (Account account : accounts) {
+            double interest = account.getBalance() * (account.getInterestRate() / 100.0);
+            account.setBalance(account.getBalance() + interest);
+            accountService.updateAccount(account);
+
+        }
+        System.out.println(" Monthly Interest Update Completed.");
+    }
+
+}
