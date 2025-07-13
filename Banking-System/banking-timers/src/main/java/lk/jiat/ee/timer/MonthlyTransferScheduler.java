e5fb26119286f755c53b99da5673a919f01a0ae4
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-13 15:11:56 +0530

    Create MonthlyTransferScheduler.java

---
+++ Banking-System/banking-timers/src/main/java/lk/jiat/ee/timer/MonthlyTransferScheduler.java
@@ -0,0 +1,62 @@
+package lk.jiat.ee.timer;
+
+import jakarta.ejb.EJB;
+import jakarta.ejb.Schedule;
+import jakarta.ejb.Singleton;
+import lk.jiat.ee.entity.Account;
+import lk.jiat.ee.entity.ScheduledTransfer;
+import lk.jiat.ee.entity.Transaction;
+import lk.jiat.ee.enums.TransactionType;
+import lk.jiat.ee.service.AccountService;
+import lk.jiat.ee.service.ScheduledTransferServissces;
+import lk.jiat.ee.service.TransactionService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Singleton
+public class MonthlyTransferScheduler {
+
+    @EJB
+    private ScheduledTransferServissces scheduledTransferServices;
+
+    @EJB
+    private TransactionService transactionService;
+    @EJB
+    private AccountService accountService;
+
+    @Schedule(dayOfMonth = "Last", hour = "23", minute = "59", persistent = false)
+//@Schedule(minute = "*/5", hour = "*", persistent = false)
+
+public void executeMonthlyTransfers() {
+        System.out.println("✅ Running monthly fund transfers");
+
+        List<ScheduledTransfer> transfers = scheduledTransferServices.getAllActiveTransfers();
+
+        for (ScheduledTransfer st : transfers) {
+            try {
+                System.out.println(st.getCustomer().getFullName());
+
+                Account account = accountService.getAccountByNumber(st.getFromAccount());
+
+                transactionService.transfer(st.getFromAccount(),st.getTargetAccount(),st.getAmount());
+
+                Transaction transaction = new Transaction();
+                transaction.setAmount(st.getAmount());
+                transaction.setDescription("Scheduled Monthly Transfer");
+                transaction.setTimestamp(LocalDateTime.now());
+                transaction.setType(TransactionType.TRANSFER);
+                transaction.setAccount(account);
+
+                transactionService.saveTransaction(transaction);
+
+                st.setActive(false); // Assuming you have a boolean field 'active'
+                scheduledTransferServices.updateScheduledTransfer(st);
+
+                System.out.println("💸 Transfer completed for customer: " + st.getCustomer().getEmail());
+            } catch (Exception e) {
+                System.err.println("❌ Transfer failed: " + e.getMessage());
+            }
+        }
+    }
+}
