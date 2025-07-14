fdabc5ad57514de4fcfb5ac73a2006b68add49e7
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-14 17:11:11 +0530

    test

---
+++ Banking-System/banking-web/src/main/java/lk/jiat/ee/servlet/Test.java
@@ -0,0 +1,35 @@
+package lk.jiat.ee.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.entity.Account;
+import lk.jiat.ee.entity.Customer;
+import lk.jiat.ee.enums.AccountType;
+import lk.jiat.ee.service.AccountService;
+import lk.jiat.ee.service.CustomerService;
+import lk.jiat.ee.service.TransactionService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+
+    @EJB
+    private AccountService accountService;
+    @EJB
+    private TransactionService transactionService;
+
+    @EJB
+    private CustomerService customerService;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+
+
+    }
+}
