bd2e8664d17679ed7637323647f7ef4d4576b31a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-15 22:00:49 +0530

    Create MailServiceProvider.java

---
+++ Banking-System/banking-core/src/main/java/lk/jiat/ee/provider/MailServiceProvider.java
@@ -0,0 +1,76 @@
+package lk.jiat.ee.provider;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.PasswordAuthentication;
+
+import lk.jiat.ee.mail.Mailable;
+import lk.jiat.ee.utils.Env;
+
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MailServiceProvider {
+
+    private Properties properties = new Properties();
+    private Authenticator authenticator;
+    private static MailServiceProvider instance;
+    private ThreadPoolExecutor executor;
+    private BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+
+    private MailServiceProvider() {
+        properties.put("mail.smtp.host", lk.jiat.ee.utils.Env.get("mailtrap.host"));
+        properties.put("mail.smtp.port", Env.get("mailtrap.port"));
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.trust", Env.get("mailtrap.host"));
+    }
+
+    public static MailServiceProvider getInstance() {
+        if (instance == null) {
+            instance = new MailServiceProvider();
+        }
+        return instance;
+    }
+
+    public void start() {
+        authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(Env.get("mailtrap.username"), Env.get("mailtrap.password"));
+            }
+        };
+
+        executor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, blockingQueue,
+                new ThreadPoolExecutor.AbortPolicy());
+        executor.prestartAllCoreThreads();
+
+        System.out.println("MailServiceProvider: Running...");
+    }
+
+    public void sendMail(Mailable mailable) {
+        blockingQueue.offer(mailable);
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public Authenticator getAuthenticator() {
+        return authenticator;
+    }
+
+    public void shutdown() {
+        if (executor != null) {
+            executor.shutdown();
+        }
+    }
+
+
+
+
+}
+
+
