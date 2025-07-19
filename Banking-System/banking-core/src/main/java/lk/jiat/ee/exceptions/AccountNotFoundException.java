2f6cd0b8b7a82912cc5066dd0cd0c6fb333c84e1
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-19 23:16:04 +0530

    Update AccountNotFoundException.java

--- Banking-System/banking-core/src/main/java/lk/jiat/ee/exceptions/AccountNotFoundException.java
+++ Banking-System/banking-core/src/main/java/lk/jiat/ee/exceptions/AccountNotFoundException.java
@@ -1,14 +1,11 @@
 package lk.jiat.ee.exceptions;
 
 public class AccountNotFoundException extends BankingSystemsException {
-
     public AccountNotFoundException(String accountNumber) {
-
         super("Account not found: "+accountNumber);
     }
 
     public AccountNotFoundException(Long accountID) {
-
         super("Account not found: "+accountID);
     }
 }
