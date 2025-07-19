4593349ef4038af4d003b8a5f67a5b3fc30d0af2
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-19 23:16:28 +0530

    Update MonthlyInterestScheduler.java

--- Banking-System/banking-timers/src/main/java/lk/jiat/ee/timer/MonthlyInterestScheduler.java
+++ Banking-System/banking-timers/src/main/java/lk/jiat/ee/timer/MonthlyInterestScheduler.java
@@ -3,29 +3,34 @@ package lk.jiat.ee.timer;
 
 import jakarta.ejb.*;
 import lk.jiat.ee.entity.Account;
+import lk.jiat.ee.exceptions.BankingSystemsException;
 import lk.jiat.ee.service.AccountService;
 
 import java.util.List;
 
     @Singleton
     public class MonthlyInterestScheduler {
-
         @EJB
         private AccountService accountService;
 
-    //    @Schedule(dayOfMonth = "Last", hour = "23", minute = "59", persistent = false)
-    //    @Schedule(minute = "*/2", hour = "*", persistent = false)
+        @Schedule(dayOfMonth = "Last", hour = "23", minute = "59", persistent = true)
         @TransactionAttribute(TransactionAttributeType.REQUIRED)
-        public void updateMonthlyInterest() {
+        public void updateMonthlyInterest() throws BankingSystemsException {
             List<Account> accounts  = accountService.getAllAccounts();
 
             for (Account account : accounts) {
                 double interest = account.getBalance() * (account.getInterestRate() / 100.0);
                 account.setBalance(account.getBalance() + interest);
-                accountService.updateAccount(account);
+                try {
+                    accountService.updateAccount(account);
+                } catch (Exception e) {
+                    throw new BankingSystemsException("Error updating account " + account.getAccountNumber(), e);
+                }
 
             }
             System.out.println(" Monthly Interest Update Completed.");
         }
 
     }
+
+
