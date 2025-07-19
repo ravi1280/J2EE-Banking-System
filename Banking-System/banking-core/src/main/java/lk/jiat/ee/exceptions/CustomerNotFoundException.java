1b7ccac2d73f22c8a12f1d26aaea82a844103c9e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-19 23:16:21 +0530

    Update CustomerNotFoundException.java

--- Banking-System/banking-core/src/main/java/lk/jiat/ee/exceptions/CustomerNotFoundException.java
+++ Banking-System/banking-core/src/main/java/lk/jiat/ee/exceptions/CustomerNotFoundException.java
@@ -3,7 +3,6 @@ package lk.jiat.ee.exceptions;
 public class CustomerNotFoundException extends BankingSystemsException {
 
     public CustomerNotFoundException(String email) {
-
         super("Customer not found: " + email);
     }
 }
